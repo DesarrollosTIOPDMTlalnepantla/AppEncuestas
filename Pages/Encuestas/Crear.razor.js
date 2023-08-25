edebaecaaf6f6b19ba1c64d27fc4d6788c9b78fa
Javier Juarez Moctezuma <desarrollo.ti@opdm.gob.mx>
2023-08-24 19:14:35 -0600

    cambios de prueba

--- Pages/Encuestas/Crear.razor.js
+++ Pages/Encuestas/Crear.razor.js
@@ -1,5 +1,6 @@
 function deshabilitarBoton() {
     let boton = document.getElementById("boton");
+    boton.innerHTML=">>>>";
     boton.style.backgroundColor = "yellow"; // o cualquier otro color
     boton.disabled=false;    
 }
